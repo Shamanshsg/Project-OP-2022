70bcff4c598525e83c60913be3b8f182924813b1
ShamanShS <SRGatyatulin@stud.kpfu.ru>
2022-05-25 14:32:29 +0300

    Немного еще подправил

--- Project.py
+++ Project.py
@@ -4,7 +4,7 @@ class RepeatException(Exception):
 
 key = []
 string = ""
-def fill (t):
+def fill (t, t1):
     global key
     global string
     enter_text = True
@@ -30,11 +30,16 @@ def fill (t):
                 enter_text = False
                 print('Введите Кол-во символов')
                 item = int(input())
-                if (len(string)%item == 0):
+                if (t1 == 1):
                     for i in range (0, len(string), item):
                         stringm.append(string[i:i+item])
                 else:
-                    stringm = block(string, key, item)
+                    if (len(string)%item == 0):
+                        for i in range (0, len(string), item):
+                            stringm.append(string[i:i+item])
+                    else:
+                        stringm = block(string, key, item)
+                print(stringm)
                 return stringm
             case "3":
                 stringm = []
@@ -60,7 +65,7 @@ def Encryption (s, k):
     if (len(s) % len(k) != 0):
         for i in range (len(k) - (len(s) % len(k))):
             s.append("\0")
-    print(s)
+    #print(s)
     str_new = [0]*len(s)
     p = 0
     for i in range (len(s)):
@@ -73,7 +78,7 @@ def reverse_key (k):
     rk = [0]*len(k)
     for i in range (len(k)):
         rk[int(k[i])] = i
-    print(rk)
+    # print(rk)
     return rk
 
 def full_str (s, k):
@@ -89,6 +94,7 @@ def full_str (s, k):
             if (new_str[i] == "0" and p):
                 new_str[i] = item
                 p = False
+    print(new_str)
     return new_str
 
 
@@ -96,17 +102,17 @@ def block (s, k, n):
     p = 0
     i2 = 0
     a = len(s) % n
-    b = len(s) // n
+    b = (len(s) // n)
     c = (((len(s) // n) + 1) // len(k))
     sm = ['']*(b+1)
     for i in range (0, len(s), n):
         if(p == (b - 1)):
             sm[p] = s[i2:i2+(len(s)%n)]
-            i2-=n - 1
+            i2-= n - 1
         else:
             sm[p] = s[i2:i2+n]
         p+=1
-        i2+=2
+        i2+=n
     print(sm)
     return sm
 
@@ -127,17 +133,17 @@ match s:
         cs = input()
         match cs:
             case "1":
-                strr = fill(cs)
+                strr = fill(cs, s)
                 str2 = Encryption(strr, key)
                 prt(str2)
 
 
             case "2":
-                strr = fill(cs)
+                strr = fill(cs, s)
                 str2 = Encryption(strr, key)
                 prt(str2)
             case "3":
-                strr = fill(cs)
+                strr = fill(cs, s)
                 str2 = Encryption(strr, key)
                 prt_word(str2)
     case 2:
@@ -147,21 +153,21 @@ match s:
         cs = input()
         match cs:
             case "1":
-                strr = fill(cs)
+                strr = fill(cs, s)
                 new_string = full_str(strr, key)
                 rekey = reverse_key(key)
                 str2 = Encryption(new_string, rekey)
                 prt(str2)
 
             case "2":
-                strr = fill(cs)
+                strr = fill(cs, s)
                 new_string = full_str(strr, key)
                 rekey = reverse_key(key)
                 str2 = Encryption(new_string, rekey)
                 prt(str2)
 
             case "3":
-                strr = fill(cs)
+                strr = fill(cs, s)
                 new_string = full_str(strr, key)
                 rekey = reverse_key(key)
                 str2 = Encryption(new_string, rekey)
